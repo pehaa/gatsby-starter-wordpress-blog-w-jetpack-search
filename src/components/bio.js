6958ebe9b6f3efe6bf71192f21bd07f31f31f011
PeHaa <info@pehaa.com>
2021-08-02 15:06:52 +0200

    remove unused imports

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,12 +1,4 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 const Bio = () => {
   return (
